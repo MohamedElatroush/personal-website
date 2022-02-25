d5aa1426675352d3298d65f10916d97fc7c5c012
MohamedElatroush <muhamed_hatem1@aucegypt.edu>
2022-02-26 00:01:03 +0200

    imag

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -12,7 +12,7 @@ const About = () => {
             <div className="container about__container">
                 <div className="about__me">
                     <div className="about__me-image">
-                        <img src={ME} alt="about image"></img>
+                        <img src={ME} alt=""></img>
                     </div>
 
                 </div>
