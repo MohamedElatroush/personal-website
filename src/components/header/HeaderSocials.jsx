403ec68f5c85f26279375fb2aa03a1baf2719e2b
MohamedElatroush <muhamed_hatem1@aucegypt.edu>
2022-02-26 00:23:16 +0200

    added LI & GH links

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -5,7 +5,7 @@ import { BsGithub } from 'react-icons/bs'
 const HeaderSocials = () => {
     return (
         <div className="header__socials">
-            <a href="https://linkedin.com" target="_blank"><FaLinkedinIn /></a>
+            <a href="https://www.linkedin.com/in/mohamedelatroush/" target="_blank"><FaLinkedinIn /></a>
             <a href="https://github.com" target="_blank"><BsGithub /></a>
         </div >
     )
