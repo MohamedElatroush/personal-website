aa4c087a3954726c16db0e5dc5a450704c170f29
MohamedElatroush <muhamed_hatem1@aucegypt.edu>
2022-02-26 20:32:13 +0200

    Hosted images on drive

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -4,7 +4,7 @@ import CV from '../../assets/MohamedElAtroush_CV.pdf'
 const CTA = () => {
     return (
         <div className="cta">
-            <a href="https://drive.google.com/uc?export=view&id=1t1rKlJ4onNYmS9T0S0pU5aUgBUgY--wG" download className="btn">Download CV</a>
+            <a href="https://drive.google.com/uc?export=view&id=1t1rKlJ4onNYmS9T0S0pU5aUgBUgY--wG" download target='_blank' className="btn">Download CV</a>
             <a href="#contact" className="btn btn-primary">Contact</a>
         </div>
     )
