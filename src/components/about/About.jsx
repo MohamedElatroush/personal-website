edc5b69335e741c463a9c2ad04934325b05da400
MohamedElatroush <muhamed_hatem1@aucegypt.edu>
2022-02-26 16:15:26 +0200

    changed react-scripts version

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -12,7 +12,7 @@ const About = () => {
             <div className="container about__container">
                 <div className="about__me">
                     <div className="about__me-image">
-                        <img src='images/me.jpeg' alt=""></img>
+                        <img src='/images/me.jpeg' alt=""></img>
                     </div>
 
                 </div>
